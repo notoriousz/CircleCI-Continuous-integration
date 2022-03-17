bc30a6cd2c8b1fddd6da0486789b0f4a517a1826
Matheus Serafim <mahserafa2016@gmail.com>
2022-03-17 18:39:17 -0300

    initial commit

---
+++ src/python/main/Operations.py
@@ -0,0 +1,6 @@
+from mimetypes import init
+
+
+class Operrations:
+    def __init__(self) -> None:
+        pass
---
+++ src/python/test/test_operations.py
@@ -0,0 +1,3 @@
+class TestOperations:
+    def __init__(self) -> None:
+        pass
